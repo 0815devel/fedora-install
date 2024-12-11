7f85fad38f86ceb40b9ca1d0424441e3eb23b409
0815devel <57812074+0815devel@users.noreply.github.com>
2024-12-11 07:38:32 +0100

    Update run.bash
    typo

--- run.bash
+++ run.bash
@@ -30,7 +30,7 @@ dnf_install "libdvdcss"
 dnf_install "rpmfusion-nonfree-release-tainted"
 dnf_install '--repo=rpmfusion-nonfree-tainted "*-firmware"'
 
-# Install VSCode
+# install VSCode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" \
 | tee /etc/yum.repos.d/vscode.repo > /dev/null
