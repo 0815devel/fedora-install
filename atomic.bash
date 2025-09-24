8c4b2716f8110048046e16c5967bd748c92eeb41
0815devel <57812074+0815devel@users.noreply.github.com>
2025-09-24 08:50:10 +0200

    Update atomic.bash

--- atomic.bash
+++ atomic.bash
@@ -11,18 +11,14 @@ for repo in free nonfree; do
 done
 
 # ------------------------
-# Update System
-# ------------------------
-dnf update -y
-
-# ------------------------
 # Multimedia-Hardware-Beschleunigung
 # ------------------------
-dnf swap -y ffmpeg-free ffmpeg --allowerasing || true
-dnf install -y intel-media-driver
-dnf update -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin || true
-dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld || true
-dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld || true
+rpm-ostree override remove fdk-aac-free libavcodec-free libavdevice-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free ffmpeg-free --install ffmpeg
+rpm-ostree install intel-media-driver
+rpm-ostree install libva-intel-driver
+rpm-ostree install gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi --allow-inactive
+rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
+rpm-ostree install mesa-vdpau-drivers-freeworld
 
 # ------------------------
 # Tainted Repos für DVD / Firmware
