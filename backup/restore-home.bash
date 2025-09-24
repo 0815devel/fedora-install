5430f0dac9068b8a98656614a7014cb6675b94c6
0815devel <57812074+0815devel@users.noreply.github.com>
2025-09-24 09:27:51 +0200

    Update restore-home.bash

--- backup/restore-home.bash
+++ backup/restore-home.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <snapshot-name>"
+    exit 1
+fi
+
 REPO="/tank/backups/home-borg-repo"
-SNAPSHOT="$1"  # z.B. "hostname-2025-09-24-1530"
+SNAPSHOT="$1"
 
 borg extract "$REPO::$SNAPSHOT"
