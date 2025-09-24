412590f332ed562920c5b372765b7e0930161eb8
0815devel <57812074+0815devel@users.noreply.github.com>
2025-09-24 08:51:09 +0200

    Update atomic.bash

--- atomic.bash
+++ atomic.bash
@@ -14,8 +14,7 @@ done
 # Multimedia-Hardware-Beschleunigung
 # ------------------------
 rpm-ostree override remove fdk-aac-free libavcodec-free libavdevice-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free ffmpeg-free --install ffmpeg
-rpm-ostree install intel-media-driver
-rpm-ostree install libva-intel-driver
+rpm-ostree install intel-media-driver libva-intel-driver
 rpm-ostree install gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi --allow-inactive
 rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld
 rpm-ostree install mesa-vdpau-drivers-freeworld
